acdffb3e4958e38b8231bc0b1fe71e887122bcf7
Nastyasiik <120024483+Nastyasiik@users.noreply.github.com>
2022-12-12 13:18:41 +0100

    Update Program.cs
    Deleted: not useful method
    Changed: methods previously created

--- Program.cs
+++ Program.cs
@@ -4,21 +4,26 @@ using System.Linq;
 namespace Tests {
     class TestApp
     {
-        //Future dynamic method
-        private static string[] AddAmountToArray(string[] array)
-        {
-            var arr = new string[array.Length * 2];
-            return arr;
-        }
         private static string[] GetArrayWithShortenedStrings(string[] arr, int numOfCharsMax)
         {
-            
+            return arr.Where(x => x.Length <= numOfCharsMax).ToArray();
         }
 
         private static string[] GetInitialArray()
         {
-            
+            Console.WriteLine("Hi, how many strings do you plan to enter?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] input = new string[n];
+
+            Console.WriteLine($"Enter your {n} strings");
+            for (int i = 0; i < n; i++)
+            {
+                var tmp = Console.ReadLine();
+                input[i] = tmp is null ? "0" : tmp;
+            }
+            return input;
         }
+        
         static void Main()
         {
             var result = GetArrayWithShortenedStrings(GetInitialArray(), 3);
