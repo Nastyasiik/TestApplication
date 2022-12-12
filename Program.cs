87dd85bad98cd743910a2c3b72847005e37ca35c
Nastyasiik <120024483+Nastyasiik@users.noreply.github.com>
2022-12-12 13:16:46 +0100

    Update Program.cs
    Added: new methods

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
+using System;
+using System.Linq;
 
+namespace Tests {
+    class TestApp
+    {
+        //Future dynamic method
+        private static string[] AddAmountToArray(string[] array)
+        {
+            var arr = new string[array.Length * 2];
+            return arr;
+        }
+        private static string[] GetArrayWithShortenedStrings(string[] arr, int numOfCharsMax)
+        {
+            
+        }
+
+        private static string[] GetInitialArray()
+        {
+            
+        }
+        static void Main()
+        {
+            var result = GetArrayWithShortenedStrings(GetInitialArray(), 3);
+
+            Console.WriteLine("Result: ");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    } 
+}
